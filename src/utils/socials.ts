cd5e652327a6cec6255ec8da5ab3507b89cf8f9b
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-18 12:33:20 -0300

    minor changes

--- src/utils/socials.ts
+++ src/utils/socials.ts
@@ -1,7 +1,8 @@
 export enum Platforms {
   LinkedIn = 'https://linkedin.com/in/jolortegui',
   GitHub = 'https://github.com/olrtg',
-  Twitter = 'https://twitter.com/olrtg_'
+  Twitter = 'https://twitter.com/olrtg_',
 }
 
 export const getSocial = (platform: Platforms) => platform
+
