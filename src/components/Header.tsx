90431ea43ec6e67d481b95525eaa4f7a5612de31
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-18 15:10:27 -0300

    hide header for print

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { useDarkTheme } from '~/utils/dark'
 const Header = () => {
   const [isDark, toggleDark] = useDarkTheme()
   return (
-    <header className="flex justify-center items-center space-x-4 pt-4">
+    <header className="flex print:hidden justify-center items-center space-x-4 pt-4">
       <button className="text-link" onClick={toggleDark}>
         { !isDark ? 'Dark' : 'Light' }
       </button>
