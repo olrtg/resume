5ea2ae0e7aaada03ddbbef50ca578a560ca14db6
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-12-11 18:55:33 -0300

    delete old project

--- src/utils/socials.ts
+++
@@ -1,7 +0,0 @@
-export enum Platforms {
-  LinkedIn = 'https://www.linkedin.com/in/olrtg',
-  GitHub = 'https://github.com/olrtg',
-  Twitter = 'https://twitter.com/olrtg_',
-}
-
-export const getSocial = (platform: Platforms) => platform
