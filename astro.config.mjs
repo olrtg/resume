c2060694674df5c8f1241135f2d1954a8c63d00e
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-12-18 16:15:23 -0300

    feat: i18n working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
+import solidJs from '@astrojs/solid-js'
 
-// https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
-});
+  integrations: [tailwind(), solidJs(), astroI18next()],
+})
